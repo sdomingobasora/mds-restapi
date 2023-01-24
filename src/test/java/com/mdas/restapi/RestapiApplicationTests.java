b4d0c18b5c24125b2ff0a645aba4209de84f4731
sdomingobasora <sdomingobasora@gmail.com>
2023-01-24 21:15:13 +0100

    feat: first commit, code from Spring Initializr

---
+++ src/test/java/com/mdas/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdas.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
