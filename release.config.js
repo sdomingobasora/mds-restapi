b044849918482cc59291c079517c0b7f7c97256d
Miquel de Domingo <miquelddg@gmail.com>
2023-02-15 20:44:29 +0100

    feat: update ci configuration

--- release.config.js
+++ release.config.js
@@ -1,27 +1,29 @@
 module.exports = {
   // TODO: Remove test-main once we are sure this works and also cleanup everything
   branches: ['main', 'test-main'],
-  // TODO: Test later
-  // plugins: [
-  //   [
-  // '@codedependant/semantic-release-docker',
-  // {
-  //   dockerTags: [
-  //     'latest',
-  //     '{{version}}',
-  //     '{{major}}-latest',
-  //     '{{major}}.{{minor}}',
-  //   ],
-  //   dockerImage: 'my-image',
-  //   dockerFile: 'Dockerfile',
-  //   dockerRegistry: 'quay.io',
-  //   dockerProject: 'codedependant',
-  //   dockerArgs: {
-  //     API_TOKEN: true,
-  //     RELEASE_DATE: new Date().toISOString(),
-  //     RELEASE_VERSION: '{{next.version}}',
-  //   },
-  // },
-  //   ],
-  // ],
+  plugins: [
+    [
+      // We have to set up
+      // - DOCKER_REGISTRY_USER
+      // - DOCKER_REGISTRY_PASSWORD
+      '@codedependant/semantic-release-docker',
+      {
+        dockerTags: [
+          'latest',
+          '{{version}}',
+          '{{major}}-latest',
+          '{{major}}.{{minor}}',
+        ],
+        dockerImage: 'mds-restapi',
+        dockerFile: 'Dockerfile',
+        dockerRegistry: 'hub.docker.com',
+        dockerProject: 'mdas',
+        dockerArgs: {
+          API_TOKEN: true,
+          RELEASE_DATE: new Date().toISOString(),
+          RELEASE_VERSION: '{{next.version}}',
+        },
+      },
+    ],
+  ],
 };
