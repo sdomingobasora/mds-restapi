b234619b482fd054ae514179d03060542355fce7
Miquel de Domingo <miquelddg@gmail.com>
2023-02-15 20:13:01 +0100

    feat: temporarily comment plugin from `release.config.js`

--- release.config.js
+++ release.config.js
@@ -1,27 +1,27 @@
 module.exports = {
   // TODO: Remove test-main once we are sure this works and also cleanup everything
   branches: ['main', 'test-main'],
-  plugins: [
-    [
-      // TODO: Test later
-      // '@codedependant/semantic-release-docker',
-      // {
-      //   dockerTags: [
-      //     'latest',
-      //     '{{version}}',
-      //     '{{major}}-latest',
-      //     '{{major}}.{{minor}}',
-      //   ],
-      //   dockerImage: 'my-image',
-      //   dockerFile: 'Dockerfile',
-      //   dockerRegistry: 'quay.io',
-      //   dockerProject: 'codedependant',
-      //   dockerArgs: {
-      //     API_TOKEN: true,
-      //     RELEASE_DATE: new Date().toISOString(),
-      //     RELEASE_VERSION: '{{next.version}}',
-      //   },
-      // },
-    ],
-  ],
+  // TODO: Test later
+  // plugins: [
+  //   [
+  // '@codedependant/semantic-release-docker',
+  // {
+  //   dockerTags: [
+  //     'latest',
+  //     '{{version}}',
+  //     '{{major}}-latest',
+  //     '{{major}}.{{minor}}',
+  //   ],
+  //   dockerImage: 'my-image',
+  //   dockerFile: 'Dockerfile',
+  //   dockerRegistry: 'quay.io',
+  //   dockerProject: 'codedependant',
+  //   dockerArgs: {
+  //     API_TOKEN: true,
+  //     RELEASE_DATE: new Date().toISOString(),
+  //     RELEASE_VERSION: '{{next.version}}',
+  //   },
+  // },
+  //   ],
+  // ],
 };
