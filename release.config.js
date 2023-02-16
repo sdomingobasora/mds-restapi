af8905c8ae2b5fd5d5f97827eb2c5fcb9c746503
Miquel de Domingo <miquelddg@gmail.com>
2023-02-16 08:21:40 +0100

    feat: cleanup `release` configuration

--- release.config.js
+++ release.config.js
@@ -1,30 +1,4 @@
 module.exports = {
-  // TODO: Remove test-main once we are sure this works and also cleanup everything
-  branches: ['main', 'test-main'],
-  plugins: [
-    ['@semantic-release/npm', { npmPublish: false }],
-    //   [
-    //     // We have to set up
-    //     // - DOCKER_REGISTRY_USER
-    //     // - DOCKER_REGISTRY_PASSWORD
-    //     '@codedependant/semantic-release-docker',
-    //     {
-    //       dockerTags: [
-    //         'latest',
-    //         '{{version}}',
-    //         '{{major}}-latest',
-    //         '{{major}}.{{minor}}',
-    //       ],
-    //       dockerImage: 'mds-restapi',
-    //       dockerFile: 'Dockerfile',
-    //       dockerRegistry: 'hub.docker.com',
-    //       dockerProject: 'mdas',
-    //       dockerArgs: {
-    //         API_TOKEN: true,
-    //         RELEASE_DATE: new Date().toISOString(),
-    //         RELEASE_VERSION: '{{next.version}}',
-    //       },
-    //     },
-    //   ],
-  ],
+  branches: ['main'],
+  plugins: [['@semantic-release/npm', { npmPublish: false }]],
 };
