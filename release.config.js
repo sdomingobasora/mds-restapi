a3de07cb8834791c1d38589c1e42e44fac2f20a1
Miquel de Domingo <miquelddg@gmail.com>
2023-02-17 09:55:16 +0100

    feat: improve semantic release configuration

--- release.config.js
+++ release.config.js
@@ -1,4 +1,24 @@
 module.exports = {
-  branches: ['main'],
-  plugins: [['@semantic-release/npm', { npmPublish: false }]],
+  branches: ['main', 'test-main'],
+  plugins: [
+    ['@semantic-release/npm', { npmPublish: false }],
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'conventionalcommits',
+        releaseRules: [
+          { type: 'BREAKING CHANGE', release: 'major' },
+          { type: 'chore', release: 'patch' },
+          { type: 'docs', release: 'patch' },
+          { type: 'feat', release: 'minor' },
+          { type: 'fix', release: 'minor' },
+          { type: 'refactor', release: 'patch' },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      { preset: 'conventionalcommits' },
+    ],
+  ],
 };
