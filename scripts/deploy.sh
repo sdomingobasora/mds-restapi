da30280fcc3d86107fb13b943f3ed717ac1fd246
sdomingobasora <sdomingobasora@gmail.com>
2023-02-18 11:34:10 +0100

    fix: get yq working

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-chmod +x ./bin/oc
-chmod +x ./bin/yq
-
-# Parse tag into YAML file using yq
 TAG=$(git describe --tags --exact-match)
-./bin/yq -i '.spec.template.spec.containers[0].image = $TAG' ./iac/deployment.yaml
+COLON_TAG=:${TAG}
+
+chmod +x ./bin/oc
+
+# Install yq and parse YAML
+YQ_VERSION="v4.18.1"
+YQ_BINARY="yq_linux_amd64"
+sudo wget https://github.com/mikefarah/yq/releases/download/$YQ_VERSION/$YQ_BINARY -O /usr/bin/yq
+sudo chmod +x /usr/bin/yq
+
+yq -i '.spec.template.spec.containers[0].image += strenv(COLON_TAG)' ./iac/deployment.yaml
 
 # Login & deploy
-# ./bin/oc login --token=$1 --server=$2
-# ./bin/oc apply -f ./iac/deployment.yaml
-# ./bin/oc apply -f ./iac/service.yaml
+./bin/oc login --token=$1 --server=$2
+./bin/oc apply -f ./iac/deployment.yaml
+./bin/oc apply -f ./iac/service.yaml
